e02128168c060aa77eb71b6fcc2ed420d00bfd3d
CL Pal <clpalmca@gmail.com>
2020-12-01 11:40:28 +0530

    added View Model

---
+++ app/src/main/java/example/kotlin/room/mvvm/repository/LoginRepository.kt
@@ -0,0 +1,31 @@
+package example.kotlin.room.mvvm.repository
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import example.kotlin.room.mvvm.model.LoginTableModel
+import example.kotlin.room.mvvm.room.LoginDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+
+class LoginRepository {
+    companion object {
+        var loginDatabase:LoginDatabase?=null
+        var loginTableModel:LiveData<LoginTableModel>?=null
+        fun initializeDB(context: Context):LoginDatabase{
+            return LoginDatabase.getDatabaseClient(context)
+        }
+        fun insertData(context: Context,username:String,password:String){
+            loginDatabase=initializeDB(context)
+            CoroutineScope(IO).launch{
+                val loginDetails=LoginTableModel(username,password)
+                loginDatabase!!.loginDao().InsertData(loginDetails)
+            }
+        }
+        fun  getLoginDetails(context: Context,username: String):LiveData<LoginTableModel>?{
+            loginDatabase=initializeDB(context)
+            loginTableModel=loginDatabase!!.loginDao().getLoginDetails(username)
+            return loginTableModel
+        }
+    }
+}
